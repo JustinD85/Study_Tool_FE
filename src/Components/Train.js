4233b246113dd9701b9c868d006f896e705a4ae6
Justin <justin@baal-software.com>
2019-01-07 23:11:16 -0700

    Implement Vocabulary filter

--- src/Components/Train.js
+++ src/Components/Train.js
@@ -1,54 +1,94 @@
 import React, { Component } from 'react';
+import Filter from './Filter';
+import uuid from 'uuid';
 
 class Train extends Component {
   constructor() {
     super();
     this.state = {
-      currentWord: null,
+      currentTopic: 'javascript',
+      currentWord: 'null',
       currentDefinition: null
     }
   }
 
-  handleSelectWord = (selectedKey, next) => {
-    const vocabulary = next || this.props.vocabulary;
+  handleSwitchFilter = (event) => {
+    let currentTopic = event.target.innerText;
+    currentTopic = this.toLowerCase(currentTopic)
 
-    vocabulary.forEach((value) => {
-      if (value instanceof Map && value.has(selectedKey)) {
-        this.setState({
-          currentDefinition: value.get(selectedKey)
-        });
-      } else if (value instanceof Map) {
-        this.handleSelectWord(selectedKey, value);
+    this.setState({ currentTopic })
+  }
+
+  toLowerCase(text) {
+    return (
+      text.replace(text[0], text[0].toLowerCase())
+        .split(' ').join('_').trim()
+    )
+  }
+
+  handleSelectWord = (currentWord = this.state.currentWord) => {
+    let { currentDefinition, currentTopic } = this.state;
+    const { topics } = this.props;
+
+    topics.get(currentTopic).forEach((section) => {
+      let didFindKey = section.get('vocabulary').has(currentWord);
+      let currentDefinition = section.get('vocabulary').get(currentWord);
+
+      if (didFindKey) {
+        let isCSSVocab = typeof currentDefinition === 'object';
+        if (isCSSVocab) {
+          currentDefinition = currentDefinition.get('description');
+        }
+        this.setState({ currentDefinition, currentWord });
       }
     });
   }
 
-  showFilteredVocab = (filterKey) => {
+  returnVocabList = (currentTopic, currentWord) => {
     let values = [];
-    const { vocabulary } = this.props;
+    const { topics } = this.props;
 
-    vocabulary.get(filterKey).forEach((value, key) => {
-      value.get('vocabulary').forEach((value, key) => {
-        values.push(<li onClick={(e) => this.handleSelectWord(e.target.innerText)}>{key}</li>)
+    topics.get(currentTopic).forEach((section) => {
+      section.get('vocabulary').forEach((definition, word) => {
+        values.push(word);
       });
+      values.sort();
     });
 
-    return values;
+    return values.map(value => (
+      <li
+        onClick={(e) => this.handleSelectWord(e.target.innerText)}
+        key={uuid()}
+        className={currentWord === value ? 'selected-word' : undefined}
+      >
+        {value}
+      </li>));
+  }
+
+  componentDidMount() {
+    this.handleSelectWord()
   }
 
   render() {
+    const { currentTopic, currentWord } = this.state;
+    const { topics } = this.props;
+
     return <article className="Train">
       <h1>Train</h1>
       <section className="vocab-list">
         <ul>
-          {this.showFilteredVocab('javascript')}
+          {this.returnVocabList(currentTopic, currentWord)}
         </ul>
       </section>
-      <section>
-        Show/Hide Answer
-      {this.state.currentDefinition && <h1>{this.state.currentDefinition} </h1>}
-      </section>
-      <section>Filter Options</section>
+
+      <details className="train-definition">
+        <summary>Show/Hide Answer</summary>
+        {this.state.currentDefinition && <h1>{this.state.currentDefinition} </h1>}
+      </details>
+      <Filter
+        topics={topics}
+        click={this.handleSwitchFilter}
+      />
     </article>
   }
 }
