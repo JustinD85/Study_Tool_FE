e3170aac56602efb5541bbbd28868c6a1602d064
Justin <justin@baal-software.com>
2019-01-09 04:43:27 -0700

    Complete testing for app

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../Components/App';
 import { shallow } from 'enzyme';
-import vocabulary from '../Data/js_fishing';
+import topics from '../Data/js_fishing';
+global.localStorage = require('../setupTests')
 
 describe('<App/>', () => {
   let wrapper;
@@ -30,4 +31,45 @@ describe('<App/>', () => {
     expect(wrapper.find('Login')).toHaveLength(1);
   });
 
+  it('should set state from sources', () => {
+    wrapper.instance().convertObjectToMap(topics);
+    expect(wrapper.state('topics')).not.toBeNull();
+  })
+
+  it('should be able to create a new User', () => {
+    wrapper.instance().handleCreateUser({ name: "pass" });
+    expect(wrapper.state('users')).toEqual({ name: "pass" });
+    expect(wrapper.find('Login')).not.toBeNull();
+  });
+
+  it('should be able go to create user screen', () => {
+    wrapper.instance().handleNewUser();
+    expect(wrapper.state('userStatus')).toEqual('createUser');
+    expect(wrapper.find('CreateUser')).not.toBeNull();
+  });
+
+  it('should allow user to login', () => {
+    wrapper.instance().handleLogin('me@me.com');
+    expect(wrapper.state('currentUser')).toEqual('me@me.com');
+    expect(wrapper.find('mainContent')).not.toBeNull();
+  });
+
+  it('should be able to track where the user is', () => {
+    wrapper.instance().handleChangeView('newStatus');
+    expect(wrapper.state('userStatus')).toEqual('newStatus');
+  });
+
+  it('should be able to change user status', () => {
+    wrapper.instance().handleChangeView('mainContent');
+    expect(wrapper.instance().changeUserStatus()).not.toBeNull();
+  });
+
+  it('should be able to save to local storage', () => {
+    wrapper.instance().saveToStorage()
+  });
+
+  it('should show loading screen if no topics', () => {
+    wrapper.setState({ 'topics': null })
+    expect(wrapper.find('.loading')).not.toBeNull();
+  })
 });
