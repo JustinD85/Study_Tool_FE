3c83bd9f9376bebfbc851e0fa00b7effabd4e991
Justin <justin@baal-software.com>
2019-01-09 04:45:01 -0700

    Complete CreateUser Testing

--- src/Tests/CreateUser.test.js
+++ src/Tests/CreateUser.test.js
@@ -4,14 +4,42 @@ import { shallow } from 'enzyme';
 
 describe('<CreateUser/>', () => {
   let wrapper;
-
+  let handleCreateUserMock = jest.fn();
+  let handleChangeViewMock = jest.fn();
+  let event = {
+    target: {
+      innerText: "Back"
+    },
+    preventDefault: () => { }
+  }
   beforeEach(() => {
     wrapper = shallow(
-      <CreateUser />
+      <CreateUser
+        handleChangeView={handleChangeViewMock}
+        handleCreateUserMock={handleCreateUserMock}
+      />
     );
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should be able to go back to login screen', () => {
+    
+    wrapper.instance().handleSubmit(event);
+    expect(handleChangeViewMock).toHaveBeenCalled();
+  });
+
+  it('should be able to create User', () => {
+    event.target.innerText = "";
+    wrapper.instance().handleSubmit(event);
+    expect(handleCreateUserMock).toHaveBeenCalled();
+  });
+
+  it('should track user form data entry', () => {
+    expect(wrapper.state('firstName')).toEqual('');
+    wrapper.instance().handleChange({ target: { 'firstName': 'Justin' } });
+    expect(wrapper.state('firstName')).toEqual('Justin');
+  })
 });
