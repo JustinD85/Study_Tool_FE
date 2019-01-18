5fe4ffc9f4f243c783bc8a3c199e8974c7fa4629
Justin <justin@baal-software.com>
2019-01-18 12:28:51 -0700

    Updates

---
+++ precache-manifest.991426bfaeab499e5b4d4c43ac1fa136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52bf2148ce6565f94b9a",
+    "url": "/Study_Tool_FE/static/css/main.226b9dcd.chunk.css"
+  },
+  {
+    "revision": "52bf2148ce6565f94b9a",
+    "url": "/Study_Tool_FE/static/js/main.52bf2148.chunk.js"
+  },
+  {
+    "revision": "50b6e35ff4df03b91b63",
+    "url": "/Study_Tool_FE/static/js/1.50b6e35f.chunk.js"
+  },
+  {
+    "revision": "4b5bcfd9de920722a7f5",
+    "url": "/Study_Tool_FE/static/js/runtime~main.4b5bcfd9.js"
+  },
+  {
+    "revision": "aecffa7ca5f32167eebc664595041526",
+    "url": "/Study_Tool_FE/index.html"
+  }
+];
